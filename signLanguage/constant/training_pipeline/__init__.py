6c9e7b5a07c7f71bcb0279c706d2ed2b8daa7dd3
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-03-29 23:38:57 +0500

    data validation added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -12,3 +12,15 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://github.com/JunaidARahat/MyTutorials/raw/main/Sign_language_data.zip"
    
+
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yaml"]
