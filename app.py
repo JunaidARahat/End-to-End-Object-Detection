ed7fbb90d10c45d36aad64523d1ba6f113fd1c8e
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-03-29 18:06:33 +0500

    data ingestion added

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from signLanguage.logger import logging
-# logging.info("welcome to the my firstlog")
-from signLanguage.exception import SignException
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
-import sys
 
-try:
 
-    a=7/'9'
-
-except Exception as e:
-    raise SignException(e,sys)
- 
+obj=TrainPipeline()
+obj.run_pipeline()        
---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+ARTIFACTS_DIR: str = "artifacts"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://github.com/JunaidARahat/MyTutorials/raw/main/Sign_language_data.zip"
+   
